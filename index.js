a7fc449d234c20b43697a646581a22b1abf3a501
Vlad <52360684+HeibaiL@users.noreply.github.com>
2019-10-07 12:21:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ let form = document.querySelector(".form");
 let input = document.querySelector(".input");
 let inputCity = document.querySelector(".city");
 let canvas = document.querySelector("#canvas");
-
 let ctx = canvas.getContext("2d");
-
-
-
+let H = canvas.height;
+let W = canvas.width;
+let arr = [];
 
   function getWeather(){
     let apikey =  "&APPID=616f1c30e7469bc62ca97046f7babe7a";
@@ -23,7 +22,6 @@ let ctx = canvas.getContext("2d");
     input.value="";
     let units= "&units=metric"
     let url = `${API}${city}${apikey}${units}`;
-
     fetch(url)
       .then(response => response.json())
       .then(data =>{
@@ -45,19 +43,18 @@ let ctx = canvas.getContext("2d");
             break;
           case "rain":
             myClass="cloud"
-            //raining();
+            raining();
             weatherText.innerHTML="rain"
             break;
           case "snow":
             weatherText.innerHTML="snow"
             myClass="snowFlake"
-            //snowing();
+            snowing();
         }
         weatherIcon.className=`fas fa-${myClass} weathericon`;
       })
-      .catch(err=>console.log("err"));
-
-};
+      .catch(err => console.log("err"));
+    };
 
 function addTime(){
   let date = new Date();
@@ -74,9 +71,71 @@ function addTime(){
   dateElement.innerHTML = day;
 };
 
+function snowing(){
+  requestAnimationFrame(snowing)
+  ctx.clearRect(0, 0, W, H);
+  ctx.fillStyle="white";
+  ctx.beginPath();
+  for(let i = 0; i<arr.length; i++){
+  let f = arr[i];
+  ctx.moveTo(f.x, f.y);
+  ctx.arc(f.x,f.y,f.r,0,Math.PI*2)
+  }
+  ctx.fill();
+  moveFlakes();
+}
+
+function moveFlakes(){
+  for (let i = 0; i<arr.length; i++){
+    let f = arr[i];
+    f.y+=f.d;
+    f.x+=f.d-0.3;
+    if(f.y>H){
+      f.y=Math.random()-10;
+    }else if(f.x>W){
+      f.x=0;
+    }
+  }
+}
+
+function addSomething(){
+  for(let i = 0; i<10; i++){
+    let x = Math.random()*W;
+    let y = Math.random()*H;
+    let r = Math.random()*4;
+    let d = Math.random();
+    arr.push({x:x, y:y-H, r:r, d:d})
+    }
+  }
+
+function raining(){
+  requestAnimationFrame(raining)
+  ctx.clearRect(0, 0, W, H);
+  ctx.fillStyle="white";
+  ctx.beginPath();
+  for(let i = 0; i<arr.length; i++){
+  let f = arr[i];
+  ctx.moveTo(f.x, f.y);
+  ctx.arc(f.x,f.y,1,0,Math.PI*2)
+  }
+  ctx.fill();
+  moveDrops();
+}
+  function moveDrops(){
+    for (let i = 0; i<arr.length; i++){
+      let f = arr[i];
+      f.y+=3;
+      f.x+=1.5;
+      if(f.y>H){
+        f.y=0;
+      }else if(f.x>W){
+        f.x=0;
+      }
+    }
+  }
 
 
-
+addSomething();
 form.addEventListener("submit",function(e){
   e.preventDefault();
   getWeather();
